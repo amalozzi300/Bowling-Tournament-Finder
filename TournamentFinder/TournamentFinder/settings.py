10544023d95ab7408575d6c47657871161cbb491
amalozzi300 <andrewmalozzi900@gmail.com>
2024-03-07 01:50:33 -0500

    used non-deprecated django-localflavor. added django-phonenumber-field[phonenumberslite] to requirements. added new requirements to INSTALLED_APPS. set a MEDIA_DIR and MEDIA_URL for uploaded image files. added media/images to project root. created bowlingcenter and tournament models and base of tournament model. registered models in admin.py. created templates/tournaments directory in tournaments app.

--- TournamentFinder/TournamentFinder/settings.py
+++ TournamentFinder/TournamentFinder/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_DIR = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'localflavor',
+    'phonenumber_field',
     'tournaments.apps.TournamentsConfig',
 ]
 
@@ -122,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PHONENUMBER_DEFAULT_REGION = 'US'
--- TournamentFinder/tournaments/models.py
+++ TournamentFinder/tournaments/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+from localflavor.us.models import USStateField, USZipCodeField
+from phonenumber_field.modelfields import PhoneNumberField
+
 # Create your models here.
+class BowlingCenter(models.Model):
+    center_name = models.CharField(max_length=75)
+    address_1 = models.CharField(max_length=128)
+    address_2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=64)
+    state = USStateField()
+    zip_code = USZipCodeField(max_length=5)
+    phone_number = PhoneNumberField()
+
+    def __str__(self):
+        return self.center_name.title()
+
+class TournamentDirector(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email_address = models.EmailField(max_length=128)
+    phone_number = PhoneNumberField()
+    profile_picture = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return f'{self.first_name.title()} {self.last_name.title()}'
+    
+class Tournament(models.Model):
+    
+
+    bowling_center = models.ForeignKey(BowlingCenter, on_delete=models.CASCADE)
+    tournament_director = models.ForeignKey(TournamentDirector, on_delete=models.CASCADE)
