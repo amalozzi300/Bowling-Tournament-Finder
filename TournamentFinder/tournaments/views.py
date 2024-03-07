241e850ae6cea3f1ec8e10a9d451d48e60666b5c
amalozzi300 <andrewmalozzi900@gmail.com>
2024-03-07 02:49:56 -0500

    added view and url to see bowling centers as a table and a test html file to do it with

--- TournamentFinder/tournaments/views.py
+++ TournamentFinder/tournaments/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+
+from .models import BowlingCenter, TournamentDirector, Tournament
 
 # Create your views here.
 def home(request):
     return HttpResponse("This is a response")
+
+def bowling_centers(request):
+    context = {'bowling_centers': BowlingCenter.objects.all()}
+    return render(request, 'tournaments/bowling_centers.html', context)
+
