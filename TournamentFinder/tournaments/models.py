2e06cbf41b7bf545cc2fb741ec4435499b05c110
amalozzi300 <andrewmalozzi900@gmail.com>
2024-03-09 01:31:39 -0500

    added website to bowlingcenter and tournament models and a flyer to tournament model. updated create and list templates as needed to accocomodate model changes. made migrations and migrated.

--- TournamentFinder/tournaments/models.py
+++ TournamentFinder/tournaments/models.py
@@ -5,12 +5,13 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class BowlingCenter(models.Model):
-    center_name = models.CharField(max_length=75)
+    center_name = models.CharField(max_length=64)
     address = models.CharField(max_length=128)
     city = models.CharField(max_length=64)
     state = USStateField()
     zip_code = USZipCodeField(max_length=5)
     phone_number = PhoneNumberField()
+    website = models.URLField(max_length=256, blank=True)
 
     def __str__(self):
         return self.center_name.title()
@@ -24,7 +25,7 @@ class TournamentDirector(models.Model):
     last_name = models.CharField(max_length=64)
     email_address = models.EmailField(max_length=128)
     phone_number = PhoneNumberField()
-    profile_picture = models.ImageField(upload_to='images', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
 
     def __str__(self):
         return f'{self.first_name.title()} {self.last_name.title()}'
@@ -38,6 +39,8 @@ class Tournament(models.Model):
     start_time = models.DateTimeField()
     bowling_center = models.ForeignKey(BowlingCenter, on_delete=models.CASCADE)
     tournament_director = models.ForeignKey(TournamentDirector, on_delete=models.CASCADE)
+    website = models.URLField(max_length=256, blank=True)
+    flyer = models.FileField(upload_to='tournament_flyers', blank=True)
 
     def __str__(self):
         return self.tournament_name
