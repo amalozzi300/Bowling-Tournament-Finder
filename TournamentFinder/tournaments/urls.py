487b8c133f61ccf2012dd279f068d224cd438826
amalozzi300 <andrewmalozzi900@gmail.com>
2024-03-16 02:29:10 -0400

    added login url path for mixin implementation

--- TournamentFinder/tournaments/urls.py
+++ TournamentFinder/tournaments/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
+    path('accounts/login/', views.login_view, name='accounts_login'),
     path('logout/', views.logout_view, name='logout'),
     path('bowling-centers', views.BowlingCenterList.as_view(), name='list_centers'),
     path('bowling-centers/create', views.BowlingCenterCreate.as_view(), name='create_center'),
