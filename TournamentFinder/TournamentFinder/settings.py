dab32fad3f187ee93f7b87c6e9ee202537e44730
amalozzi300 <andrewmalozzi900@gmail.com>
2024-03-30 21:30:17 -0400

    removed bootstrap5 from installed apps list

--- TournamentFinder/TournamentFinder/settings.py
+++ TournamentFinder/TournamentFinder/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
     'localflavor',
     'phonenumber_field',
     'tournaments.apps.TournamentsConfig',
