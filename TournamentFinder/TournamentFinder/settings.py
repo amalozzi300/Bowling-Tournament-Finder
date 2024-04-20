8d464b27e28902cfbc21b7f513d95d403986928f
amalozzi300 <andrewmalozzi900@gmail.com>
2024-04-20 15:15:27 -0400

    edit sttngs to use postgis engine + chngd req.txt

--- TournamentFinder/TournamentFinder/settings.py
+++ TournamentFinder/TournamentFinder/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'TournamentFinder.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
