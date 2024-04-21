a14c187a3970814966521bd86fee1472e0185e64
amalozzi300 <andrewmalozzi900@gmail.com>
2024-04-21 14:45:48 -0400

    updated project name to bowlingmafia

--- BowlingMafia/BowlingMafia/asgi.py
+++ BowlingMafia/BowlingMafia/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TournamentFinder project.
+ASGI config for bowlingmafia project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TournamentFinder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bowlingmafia.settings')
 
 application = get_asgi_application()
--- BowlingMafia/BowlingMafia/wsgi.py
+++ BowlingMafia/BowlingMafia/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TournamentFinder project.
+WSGI config for bowlingmafia project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TournamentFinder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bowlingmafia.settings')
 
 application = get_wsgi_application()
